628d320563f79029d324806864305cc3ee8fbfd6
Jack Anderson <jack.anderson@salesagility.com>
2025-01-08 14:41:21 +0000

    Add Email Marketing support on Campaign charts

--- core/modules/Campaigns/Statistics/CampaignResponseByRecipientType.php
+++ core/modules/Campaigns/Statistics/CampaignResponseByRecipientType.php
@@ -89,7 +89,12 @@ class CampaignResponseByRecipientType extends LegacyHandler implements Statistic
     {
         [$module, $id, $criteria, $sort] = $this->extractContext($query);
 
-        if (empty($module) || $module !== 'campaigns') {
+        $allowedModules = [
+          'email-marketing',
+          'campaigns'
+        ];
+
+        if (empty($module) || !in_array($module, $allowedModules, true)) {
             return $this->getEmptySeriesResponse(self::KEY);
         }
 
@@ -102,11 +107,18 @@ class CampaignResponseByRecipientType extends LegacyHandler implements Statistic
             'removed' => 'LBL_LOG_ENTRIES_REMOVED_TITLE',
         ];
 
+        $emailMarketingId = null;
+
         $this->init();
         $this->startLegacyApp();
 
         $legacyName = $this->moduleNameMapper->toLegacy($module);
         $bean = BeanFactory::newBean($legacyName);
+
+        if ($module === 'email-marketing') {
+            $emailMarketingId = $id;
+            $id = $this->getCampaignId($id);
+        }
 
         if (!$bean instanceof SugarBean) {
             return $this->getEmptySeriesResponse(self::KEY);
@@ -114,7 +126,7 @@ class CampaignResponseByRecipientType extends LegacyHandler implements Statistic
 
         $query = $this->queryHandler->getQuery($bean, $criteria, $sort);
 
-        $query = $this->generateQuery($query, $id, $activities);
+        $query = $this->generateQuery($query, $id, $activities, $emailMarketingId);
 
         $result = $this->runQuery($query, $bean);
 
@@ -133,13 +145,17 @@ class CampaignResponseByRecipientType extends LegacyHandler implements Statistic
     }
 
 
-    public function generateQuery(array $query, string $id, $activities): array
+    public function generateQuery(array $query, string $id, $activities, string $emailMarketingId = null): array
     {
         $query['select'] = "SELECT activity_type,target_type, count(*) hits ";
         $query['from'] = " FROM campaign_log ";
         $query['where'] = " WHERE campaign_id = '$id' AND archived=0 AND deleted=0 AND activity_type in ('" . implode("','", array_keys($activities)) . "')";
         $query['group_by'] = " GROUP BY  activity_type, target_type";
         $query['order_by'] = " ORDER BY  activity_type, target_type";
+
+        if ($emailMarketingId !== null) {
+            $query['where'] .= " AND marketing_id ='$emailMarketingId'";
+        }
 
         return $query;
     }
@@ -152,5 +168,24 @@ class CampaignResponseByRecipientType extends LegacyHandler implements Statistic
     protected function runQuery(array $query, $bean): array
     {
         return $this->queryHandler->runQuery($bean, $query, -1, -2);
+    }
+
+    protected function getCampaignId(string $id): string
+    {
+        $bean = BeanFactory::newBean('Campaigns');
+        $query = [];
+        $query['select'] = 'SELECT campaign_id';
+        $query['from'] = ' FROM email_marketing';
+        $query['where'] = " WHERE id = '$id'";
+
+        $result = $this->runQuery($query, $bean);
+
+        $campaignId = null;
+
+        foreach ($result as $row) {
+            $campaignId = $row['campaign_id'];
+        }
+
+        return $campaignId;
     }
 }
