f6b7c4c57a53a3ee64b2cb39d9f45a3478b16af8
Jack Anderson <jack.anderson@salesagility.com>
2025-02-24 14:48:03 +0000

    Add Send Email Handler

---
+++ core/backend/Emails/LegacyHandler/SendEmailHandler.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * SuiteCRM is a customer relationship management program developed by SalesAgility Ltd.
+ * Copyright (C) 2025 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SALESAGILITY, SALESAGILITY DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Supercharged by SuiteCRM" logo. If the display of the logos is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Supercharged by SuiteCRM".
+ */
+
+
+namespace App\Emails\LegacyHandler;
+
+use App\Engine\LegacyHandler\LegacyHandler;
+use App\Engine\LegacyHandler\LegacyScopeState;
+use Psr\Log\LoggerInterface;
+use SugarPHPMailer;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class SendEmailHandler extends LegacyHandler {
+
+    protected const HANDLER_KEY = 'send-email';
+
+    protected LoggerInterface $logger;
+
+    public function __construct(
+        string $projectDir,
+        string $legacyDir,
+        string $legacySessionName,
+        string $defaultSessionName,
+        LegacyScopeState $legacyScopeState,
+        RequestStack $requestStack,
+        LoggerInterface $logger
+    )
+    {
+        parent::__construct(
+            $projectDir,
+            $legacyDir,
+            $legacySessionName,
+            $defaultSessionName,
+            $legacyScopeState,
+            $requestStack
+        );
+        $this->logger = $logger;
+    }
+
+    public function getHandlerKey(): string
+    {
+        return self::HANDLER_KEY;
+    }
+
+    public function sendEmail(SugarPHPMailer $email, $isTest = false): bool {
+
+        $sent = $email->send();
+
+        if ($isTest && $sent) {
+            return true;
+        }
+
+        if (!$sent) {
+            $recipients = array_keys($email->getAllRecipientAddresses());
+
+            foreach ($recipients as $recipient) {
+                $this->logger->error("Failed to send email to " . $recipient);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
