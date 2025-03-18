ebb303f2ea357aa172681dbfadef66a762f096a2
Jack Anderson <jack.anderson@salesagility.com>
2025-03-18 13:48:46 +0000

    Fix Email not showing in subpanel

--- core/backend/Emails/LegacyHandler/EmailProcessProcessor.php
+++ core/backend/Emails/LegacyHandler/EmailProcessProcessor.php
@@ -29,10 +29,12 @@
 namespace App\Emails\LegacyHandler;
 
 use App\Data\Entity\Record;
+use App\Data\LegacyHandler\PreparedStatementHandler;
 use App\Data\Service\RecordProviderInterface;
 use App\Engine\LegacyHandler\LegacyHandler;
 use App\Engine\LegacyHandler\LegacyScopeState;
 use PHPMailer\PHPMailer\Exception;
+use SugarEmailAddress;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class EmailProcessProcessor extends LegacyHandler
@@ -40,6 +42,7 @@ class EmailProcessProcessor extends LegacyHandler
 
     protected SendEmailHandler $sendEmailHandler;
     protected RecordProviderInterface $recordProvider;
+    protected PreparedStatementHandler $preparedStatementHandler;
 
     public function __construct(
         string $projectDir,
@@ -49,7 +52,8 @@ class EmailProcessProcessor extends LegacyHandler
         LegacyScopeState $legacyScopeState,
         RequestStack $requestStack,
         SendEmailHandler $sendEmailHandler,
-        RecordProviderInterface $recordProvider
+        RecordProviderInterface $recordProvider,
+        PreparedStatementHandler $preparedStatementHandler
     ) {
         parent::__construct(
             $projectDir,
@@ -61,6 +65,7 @@ class EmailProcessProcessor extends LegacyHandler
         );
         $this->sendEmailHandler = $sendEmailHandler;
         $this->recordProvider = $recordProvider;
+        $this->preparedStatementHandler = $preparedStatementHandler;
     }
 
     public function getHandlerKey(): string
@@ -72,7 +77,7 @@ class EmailProcessProcessor extends LegacyHandler
      * @param Record $emailRecord
      * @param bool $isTest
      * @return array
-     * @throws Exception
+     * @throws \Doctrine\DBAL\Exception
      */
     public function processEmail(Record $emailRecord, bool $isTest = false): array
     {
@@ -114,7 +119,14 @@ class EmailProcessProcessor extends LegacyHandler
             ];
         }
 
-        $this->recordProvider->saveRecord($emailRecord);
+        $addresses = [];
+
+        [$addresses['to'], $addresses['cc'], $addresses['bcc']] = $this->mapAttributes($emailAttributes);
+
+        $emailRecord = $this->recordProvider->saveRecord($emailRecord);
+
+        $this->saveEmailAddresses($outboundRecord->getAttributes(), $emailRecord->getAttributes(), $addresses);
+
         $this->close();
         return [
             'success' => true,
@@ -151,5 +163,116 @@ class EmailProcessProcessor extends LegacyHandler
         return [];
     }
 
+    /**
+     * @param $emailAttributes
+     * @return array[]
+     */
+    protected function mapAttributes($emailAttributes): array
+    {
+        $to = [];
+        $bcc = [];
+        $cc = [];
+
+        $toAddresses = !empty($recordAttributes['to_addrs_names']) ? $recordAttributes['to_addrs_names'] : [];
+        $ccAddresses = !empty($recordAttributes['cc_addrs_names']) ? $recordAttributes['cc_addrs_names'] : [];
+        $bccAddresses = !empty($recordAttributes['bcc_addrs_names']) ? $recordAttributes['bcc_addrs_names'] : [];
+
+        foreach ($toAddresses as $key => $value) {
+            $to[] = $value['email1'] ?? $value['email'];
+        }
+        foreach ($ccAddresses as $key => $value) {
+            $cc[] = $value['email1'] ?? $value['email'];
+        }
+        foreach ($bccAddresses as $key => $value) {
+            $bcc[] = $value['email1'] ?? $value['email'];
+        }
+
+        return [$to, $cc, $bcc];
+    }
+
+    /**
+     * @param array|null $outboundAttributes
+     * @param array $emailAttributes
+     * @param $addresses
+     * @return void
+     * @throws \Doctrine\DBAL\Exception
+     */
+    protected function saveEmailAddresses(?array $outboundAttributes, array $emailAttributes, $addresses): void
+    {
+
+        $id = $emailAttributes['id'] ?? null;
+        $emailId = $this->getEmailId($outboundAttributes['smtp_from_addr']);
+
+        if (!empty($emailId)) {
+            $this->linkEmailToAddress($id, $emailId, 'from');
+        }
+
+        $this->mapAddresses($id, $addresses['to'], 'to');
+        $this->mapAddresses($id, $addresses['cc'], 'cc');
+        $this->mapAddresses($id, $addresses['bcc'], 'bcc');
+    }
+
+
+    /**
+     * @param $emailId
+     * @param $email
+     * @param $type
+     * @return void
+     * @throws \Doctrine\DBAL\Exception
+     */
+    protected function linkEmailToAddress($emailId, $email, $type): void
+    {
+        $records = $this->preparedStatementHandler->fetch(
+            "SELECT * FROM emails_email_addr_rel
+         WHERE email_id = :email_id AND email_address_id = :email_address_id AND address_type = :type AND deleted = 0",
+        ['email_id' => $emailId, 'email_address_id' => $email, 'type' => $type],
+        [
+            ['param' => 'email_id', 'type' => 'string'],
+            ['param' => 'email_address_id', 'type' => 'string'],
+            ['param' => 'type', 'type' => 'string']
+        ]);
+
+        if (empty($records)) {
+            $id = create_guid();
+
+            $this->preparedStatementHandler->update(
+                'INSERT INTO emails_email_addr_rel VALUES(:id, :email_id, :type, :email, 0)',
+                ['id' => $id, 'email_id' => $emailId, 'email' => $email, 'type' => $type],
+                [
+                    ['param' => 'email_id', 'type' => 'string'],
+                    ['param' => 'id', 'type' => 'string'],
+                    ['param' => 'email', 'type' => 'string'],
+                    ['param' => 'type', 'type' => 'string']
+                ]
+            );
+        }
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    protected function getEmailId(string $value): string
+    {
+        $sugarEmailAddresses = new SugarEmailAddress();
+        return $sugarEmailAddresses->getEmailGUID($value) ?? '';
+    }
+
+    /**
+     * @param $id
+     * @param $address
+     * @param $type
+     * @return void
+     * @throws \Doctrine\DBAL\Exception
+     */
+    protected function mapAddresses($id, $address, $type): void
+    {
+        foreach ($address as $key => $value) {
+            $emailId = $this->getEmailId($value);
+            if (!empty($emailId)) {
+                $this->linkEmailToAddress($id, $emailId, $type);
+            }
+        }
+    }
 
 }
