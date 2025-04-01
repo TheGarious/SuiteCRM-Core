df1deff104be906e973aef14f4b7552d2e9b4b76
Jack Anderson <jack.anderson@salesagility.com>
2025-04-01 08:50:55 +0100

    [Legacy] Add Sent Status

--- public/legacy/include/language/en_us.lang.php
+++ public/legacy/include/language/en_us.lang.php
@@ -981,7 +981,8 @@ $app_list_strings = array(
         '' => '',
         'active' => 'Active',
         'inactive' => 'Inactive',
-        'scheduled' => 'Scheduled'
+        'scheduled' => 'Scheduled',
+        'sent' => 'Sent'
     ),
     'email_marketing_type_dom' => array(
         'marketing' => 'Marketing',
