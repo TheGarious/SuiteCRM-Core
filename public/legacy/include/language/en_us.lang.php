39526a3fe6f37a91979b4ae5b5386aa5c20fdfce
Clemente Raposo <clemente.raposo@salesagility.com>
2025-04-21 14:43:03 +0100

    [Legacy] Add EmailMarketing related labels to app_strings

--- public/legacy/include/language/en_us.lang.php
+++ public/legacy/include/language/en_us.lang.php
@@ -970,7 +970,7 @@ $app_list_strings = array(
         'active' => 'Active',
         'draft' => 'Draft',
         'scheduled' => 'Scheduled',
-        'in_queue' => 'In Queue',
+        'pending_send' => 'Pending Send',
         'aborted' => 'Aborted',
         'sending' => 'Sending',
         'sent' => 'Sent'
@@ -979,6 +979,11 @@ $app_list_strings = array(
         'email' => 'Prevent Duplicate Records to single Email',
         'none' => 'None',
     ],
+    'email_marketing_queueing_status_dom' => array(
+        'not_started' => 'Not Started',
+        'in_progress' => 'In Progress',
+        'finished' => 'Finished',
+    ),
     'email_marketing_type_dom' => array(
         'marketing' => 'Marketing',
         'survey' => 'Survey',
@@ -4166,3 +4171,6 @@ $app_strings['LBL_COLOR_GRAY'] = 'Gray';
 $app_strings['LBL_COLOR_DARK_GRAY'] = 'Dark Gray';
 $app_strings['LBL_COLOR_LIGHT_GRAY'] = 'Light Gray';
 $app_strings['LBL_COLOR_WHITE'] = 'White';
+
+$app_strings['LBL_EMAIL_MARKETING_NOT_UNSCHEDULED'] = 'Email Marketing is not Un Scheduled cannot schedule';
+$app_strings['LBL_INCORRECT_RECORD_ID'] = 'Incorrect record id';
