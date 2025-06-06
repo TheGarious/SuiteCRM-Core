af357fa221da93058fe4eeb94ffbc99646a598f7
Clemente Raposo <clemente.raposo@suitecrm.com>
2025-06-06 17:09:39 +0000

    [Legacy] Bump version to 8.9.0-beta

--- public/legacy/suitecrm_version.php
+++ public/legacy/suitecrm_version.php
@@ -3,6 +3,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '8.8.1';
+$suitecrm_version = '8.9.0-beta';
 $suitecrm_timestamp = '2025-08-06 12:00:00';
 $suitecrm_legacy = '7.14.7';
