bdd06235baf717511a175d51b95c5b9f26449ef9
Jack Anderson <jack.anderson@salesagility.com>
2025-06-16 08:49:23 +0100

    Fix unsubscribe unformatting monaco editor when injected

--- core/app/core/src/lib/fields/squire/templates/edit/squire.component.ts
+++ core/app/core/src/lib/fields/squire/templates/edit/squire.component.ts
@@ -747,9 +747,18 @@ export class SquireEditFieldComponent extends BaseFieldComponent implements OnDe
             titleKey: 'LBL_INJECT_UNSUBSCRIBE',
             klass: 'squire-editor-button btn btn-sm',
             onClick: () => {
+
+                const unsubscribeLabel = this.language.getFieldLabel('LBL_UNSUBSCRIBE') || 'Unsubscribe';
+
+                if (this.editorMode() === 'code') {
+                    this.monacoEditor.editor.trigger('keyboard', 'type',
+                        {text: '<a title="' + '{{ unsubscribe_link }}' + '" href="{{ unsubscribe_link }}">' + unsubscribeLabel + '</a>'}
+                    );
+                    return;
+                }
+
                 if (this.editor.getSelectedText() === '') {
-                    const unsubscribeLabel = this.language.getFieldLabel('LBL_UNSUBSCRIBE') || 'Unsubscribe';
-                    this.editor.insertHTML('<a title="' + '{{ unsubscribe_link }}'  +'" href="{{ unsubscribe_link }}">' + unsubscribeLabel + '</a>');
+                    this.editor.insertHTML('<a title="' + '{{ unsubscribe_link }}' + '" href="{{ unsubscribe_link }}">' + unsubscribeLabel + '</a>');
                     return;
                 }
 
