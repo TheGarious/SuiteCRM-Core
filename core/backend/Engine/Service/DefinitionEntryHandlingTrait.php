76b5f33c1ab103eabdd2f6ac1a2ff61a762d1bb8
Jack Anderson <jack.anderson@salesagility.com>
2025-06-18 14:41:15 +0100

    Fix Insights button position

--- core/backend/Engine/Service/DefinitionEntryHandlingTrait.php
+++ core/backend/Engine/Service/DefinitionEntryHandlingTrait.php
@@ -54,7 +54,7 @@ trait DefinitionEntryHandlingTrait
         $exclude = $moduleEntryConfig['exclude'] ?? [];
         $moduleEntries = $moduleEntryConfig[$entryName] ?? [];
 
-        $entries = array_merge($defaultEntries, $moduleEntries);
+        $entries = array_merge($moduleEntries, $defaultEntries);
         $filteredEntries = [];
         foreach ($entries as $entryKey => $entry) {
             if (in_array($entryKey, $exclude, true)) {
