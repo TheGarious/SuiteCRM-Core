52654ae2480644fe64455f336cae87b7a2bb018c
Clemente Raposo <clemente.raposo@suitecrm.com>
2025-07-22 16:00:50 +0100

    Fix file detail mode link

--- core/app/core/src/lib/fields/file/templates/detail/file.component.ts
+++ core/app/core/src/lib/fields/file/templates/detail/file.component.ts
@@ -84,7 +84,7 @@ export class FileDetailFieldComponent extends BaseFieldComponent {
             name: valueObject?.attributes?.original_name ?? '',
             size: valueObject?.attributes?.size ?? 0,
             type: valueObject?.attributes?.type ?? '',
-            url: './' + valueObject?.attributes?.contentUrl || '',
+            url: '.' + valueObject?.attributes?.contentUrl || '',
             status: signal('saved'),
             progress: signal(100),
             dateCreated: valueObject?.attributes?.date_entered || ''
