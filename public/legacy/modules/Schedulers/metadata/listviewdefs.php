264dcc3843ed8682a19a26ac549a439c7db747c7
Jack Anderson <jack.anderson@salesagility.com>
2025-09-23 13:48:32 +0100

    Fix #735 - [Legacy] Update Scheduler list defs

--- public/legacy/modules/Schedulers/metadata/listviewdefs.php
+++ public/legacy/modules/Schedulers/metadata/listviewdefs.php
@@ -42,35 +42,31 @@ if (!defined('sugarEntry') || !sugarEntry) {
  */
 
 
-$listViewDefs ['Schedulers'] =
-array(
-  'NAME' =>
-  array(
-    'width' => '35%',
-    'label' => 'LBL_LIST_NAME',
-    'link' => true,
-    'sortable' => true,
-    'default' => true,
-  ),
-  'JOB_INTERVAL' =>
-  array(
-    'width' => '20%',
-    'label' => 'LBL_LIST_JOB_INTERVAL',
-    'default' => true,
-    'sortable' => false,
-  ),
-  'DATE_TIME_START' =>
-  array(
-    'width' => '25%',
-    'label' => 'LBL_LIST_RANGE',
-    'customCode' => '{$DATE_TIME_START} - {$DATE_TIME_END}',
-    'default' => true,
-    'related_fields' => array('date_time_end'),
-  ),
-  'STATUS' =>
-  array(
-    'width' => '15%',
-    'label' => 'LBL_LIST_STATUS',
-    'default' => true,
-  ),
-);
+$listViewDefs ['Schedulers'] = [
+    'NAME' => [
+        'width' => '35%',
+        'label' => 'LBL_LIST_NAME',
+        'link' => true,
+        'sortable' => true,
+        'default' => true,
+    ],
+    'JOB_INTERVAL' => [
+        'width' => '20%',
+        'label' => 'LBL_LIST_JOB_INTERVAL',
+        'default' => true,
+        'sortable' => false,
+    ],
+    'DATE_TIME_START' => [
+        'width' => '25%',
+        'label' => 'LBL_LIST_RANGE',
+        'customCode' => '{$DATE_TIME_START} - {$DATE_TIME_END}',
+        'type' => 'varchar',
+        'default' => true,
+        'related_fields' => ['date_time_end'],
+    ],
+    'STATUS' => [
+        'width' => '15%',
+        'label' => 'LBL_LIST_STATUS',
+        'default' => true,
+    ],
+];
